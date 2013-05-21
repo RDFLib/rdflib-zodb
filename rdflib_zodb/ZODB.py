ab739b7f5c5440a3a7d78cb377c4f32d4400409e
Gerhard Weis <gerhard.weis@gmail.com>
2013-05-21 10:28:22 +1000

    drop python 2.4 and 2.5 support add python 3 support

--- rdflib_zodb/ZODB.py
+++ rdflib_zodb/ZODB.py
@@ -6,12 +6,7 @@ from rdflib.plugins.memory import IOMemory
 # you must export your PYTHONPATH to point to a Z2.8 or Z3+ installation to 
 # get this to work!, like: export PYTHONPATH="/home/michel/dev/Zope3Trunk/src"
 
-try:
-    # Zope 3
-    from persistent import Persistent
-except ImportError:
-    # < Zope 2.8?
-    from Persistence import Persistent
+from persistent import Persistent
 
 from BTrees.IOBTree import IOBTree
 from BTrees.OIBTree import OIBTree
